30c3da62c2241b43d870c601a384b5b674b12dd9
Kaushik-Junnuri <kaushikjunnuri@gmail.com>
2023-06-04 03:16:54 -0500

    created folders and data collection files

---
+++ template.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctime)s]: %(message)s:')
+
+project_name='Usa_elections'
+
+list_of_files = [
+    ".github/workflows/.gitkeep", #like a placholder since empty folders cannot be pushed
+    f'src/{project_name}/__init__.py',
+    f'src/{project_name}/components/__init__.py',
+    f'src/{project_name}/utils/__init__.py',
+    f'src/{project_name}/utils/common.py',
+    f'src/{project_name}/logging/__init__.py',
+    f'src/{project_name}/config/__init__.py',
+    f'src/{project_name}/config/configuration.py',
+    f'src/{project_name}/pipeline/__init__.py',
+    f'src/{project_name}/entity/__init__.py',
+    f'src/{project_name}/constants/__init__.py',
+    "config/config.yaml",
+    "params.yaml",
+    "app.py",
+    "main.py",
+    "DockerFile",
+    "requirements.txt",
+    "setup.py",
+    "research/experiment.ipynb"
+]
+
+for file_path in list_of_files:
+    filepath= Path(file_path)
+    #create folder if it doesn't exist
+    filedir,file_name = os.path.split(filepath)
+    if filedir!= "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Created folder - {filedir} and file - {file_name}")
+    
+    if  (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"created empty file - {filepath}")
+    else:
+        logging.info(f"file already exists- {filepath}")
+
